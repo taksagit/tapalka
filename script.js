4906eef08563db735736c09fc3ab2f9dc530ee5b
taksa <35803470+taksagit@users.noreply.github.com>
2025-02-17 12:52:35 +0300

    script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ let tg = window.Telegram.WebApp;
 tg.expand(); // Разворачиваем приложение
 
 // Загрузка количества аркоинов из localStorage
-let score = localStorage.getItem('arc_coins') || 0;
-document.getElementById('score').textContent = score;
+let score = parseFloat(localStorage.getItem('arc_coins')) || 0;
+document.getElementById('score').textContent = score.toFixed(2);
 
 // Обработчик клика по герою
 document.getElementById('hero').addEventListener('click', function () {
     score += 0.01;
-    document.getElementById('score').textContent = score;
-    localStorage.setItem('arc_coins', score);
+    document.getElementById('score').textContent = score.toFixed(2);
+    localStorage.setItem('arc_coins', score.toFixed(2));
 });
